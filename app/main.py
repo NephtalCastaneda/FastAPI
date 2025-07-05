b50d96bae057238d2801c1bc66308e6bcca9f9d3
NephtalCastaneda <nephtali.castaneda@gmail.com>
2025-07-04 18:43:20 -0600

    Segundo Intento de Cargar Api

--- app/main.py
+++ app/main.py
@@ -14,7 +14,7 @@ class ProyectoInput(BaseModel):
 
 @app.get("/")
 def raiz():
-    return {"mensaje": "Hola Mundo para API"}
+    return {"mensaje": "Hola Mundo Segundo Intento para API"}
 
 # -----------------------------
 # 1. PREDECIR CON MODELO
